5bc39fec358ace8a913dac902a5428ad38bbae6f
Jose Plana <jplana@gmail.com>
2013-10-26 00:25:42 +0200

    Skip fix if not running in travis

--- build_etcd.sh
+++ build_etcd.sh
@@ -4,5 +4,8 @@ cd etcd
 git checkout v0.1.2
 ./build
 
+
+${TRAVIS:?"This is not a Travis build. All Done"}
 #Temporal solution to travis issue #155
 sudo rm -rf /dev/shm && sudo ln -s /run/shm /dev/shm
+echo "All Done"
