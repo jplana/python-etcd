a71a56171ca00c0d711bc709bf929500b5d34556
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-10-01 18:01:05 +0200

    Correct install requirements to depend on the python version

--- setup.py
+++ setup.py
@@ -8,9 +8,15 @@ NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 version = '0.4.1'
 
+# Dnspython is two different packages depending on python version
+if sys.version_info.major == 2:
+    dns = 'dnspython'
+else:
+    dns = 'dnspython3'
+
 install_requires = [
     'urllib3>=1.7',
-    'dnspython'
+    dns
 ]
 
 test_requires = [
