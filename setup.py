d28f9a2fea8504fda93884e15955e185aac3c197
Matthias Urlichs <matthias@urlichs.de>
2015-11-11 13:11:55 +0100

    Use pytest for running tests

--- setup.py
+++ setup.py
@@ -16,6 +16,26 @@ test_requires = [
     'pytest',
 ]
 
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 setup(
     name='aioetcd',
     version=version,
@@ -39,4 +59,5 @@ setup(
     install_requires=install_requires,
     tests_require=test_requires,
     test_suite='pytest.collector',
+    cmdclass = {'test': PyTest},
 )
