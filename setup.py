0129a43e1abe66b2844e97443e8c1ee9f36527a6
Giuseppe Lavagetto <lavagetto@gmail.com>
2015-12-02 18:18:24 +0100

    Release 0.4.3
    Enough changes were important enough to grant a release, namely python
    3.5 compatibility and authentication/ACLs.
    
    Also a ton of fixes that people would probably love to have.
    
    Added an AUTHORS file to acknowledge openly the work of all the
    contributors to the project.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.4.2'
+version = '0.4.3'
 
 # Dnspython is two different packages depending on python version
 if sys.version_info.major == 2:
