b9990019c5c0608713bb6dcc013c08dbf31a9628
Giuseppe Lavagetto <lavagetto@gmail.com>
2017-02-13 08:30:34 +0100

    Update etcd, urllib3, pyopenssl versions in travis builds
    Specifically, use the latest pyopenssl, a fairly recent urllib3, and the
    most recent version of etcd 2.x

--- build_etcd.sh
+++ build_etcd.sh
@@ -9,10 +9,16 @@ fi
 
 echo "Using ETCD version $ETCD_VERSION"
 
+BASE=$PWD
+mkdir -p gopath/src/coreos/
+export GOPATH=$BASE/gopath/
+cd $GOPATH/src/coreos
 git clone https://github.com/coreos/etcd.git
 cd etcd
-git checkout $ETCD_VERSION
+git checkout -b buildout $ETCD_VERSION
 ./build
+cd $BASE
+cp -r $GOPATH/src/coreos/etcd/bin .
 
 
 ${TRAVIS:?"This is not a Travis build. All Done"}
