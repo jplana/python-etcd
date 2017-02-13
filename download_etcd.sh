c99fe8bf39113333b2999ec32d564052238ddb82
Giuseppe Lavagetto <lavagetto@gmail.com>
2017-02-13 08:30:34 +0100

    Update etcd, urllib3, pyopenssl versions in travis builds
    Specifically, use the latest pyopenssl, a fairly recent urllib3, and the
    most recent version of etcd 2.x

---
+++ download_etcd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+VERSION=${1:-2.3.7}
+mkdir -p bin
+URL="https://github.com/coreos/etcd/releases/download/v${VERSION}/etcd-v${VERSION}-linux-amd64.tar.gz"
+curl -L $URL | tar -C ./bin --strip-components=1 -xzvf - "etcd-v${VERSION}-linux-amd64/etcd"
