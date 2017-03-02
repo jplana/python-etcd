96f2ff4e1d5143d617e0f9c2c57eb08fd3b2ea03
Jose Plana <jplana@gmail.com>
2017-03-03 00:03:02 +0100

    Prepare release 0.4.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.4.4'
+version = '0.4.5'
 
 install_requires = [
     'urllib3>=1.7.1',
