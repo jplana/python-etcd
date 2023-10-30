dcb22ef1492bb9bd4f5dd0098c6586abc53854a3
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2023-10-30 16:57:17 +0100

    Try to fix etcd path

--- download_etcd.sh
+++ download_etcd.sh
@@ -4,3 +4,4 @@ VERSION=${1:-2.3.7}
 mkdir -p bin
 URL="https://github.com/coreos/etcd/releases/download/v${VERSION}/etcd-v${VERSION}-linux-amd64.tar.gz"
 curl -L $URL | tar -C ./bin --strip-components=1 -xzvf - "etcd-v${VERSION}-linux-amd64/etcd"
+mv bin/etcd /usr/local/bin/
